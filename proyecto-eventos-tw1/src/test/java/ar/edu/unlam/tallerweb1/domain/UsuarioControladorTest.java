783904e8259f557d7bcfd60a972facd5b5d45331
Duilio Rubio <111440362+DuRubio@users.noreply.github.com>
2023-05-07 11:19:42 -0300

    primer commit

---
+++ proyecto-eventos-tw1/src/test/java/ar/edu/unlam/tallerweb1/domain/UsuarioControladorTest.java
@@ -0,0 +1,42 @@
+package ar.edu.unlam.tallerweb1.domain;
+
+import org.junit.Test;
+
+public class UsuarioControladorTest {
+
+
+    @Test
+    public void queSePuedaRegistrarUnUsuarioConDatosCorrectosYLoLleveALogin(){
+
+    }
+
+    @Test
+    public void queNoSePuedaRegistrarUnUsuarioConMailIncorrectoYEnvieMensaje(){
+
+    }
+    @Test
+    public void queNoSePuedaRegistrarUnUsuarioConClaveIncorrectoYEnvieMensaje(){
+
+    }
+
+    @Test
+    public void queSePuedaLogearConDatosCorrectosYLoLleveAHome(){
+
+    }
+
+    @Test
+    public void queNoSePuedaLogearConDatosIncorrectosYLoLleveARegistrarse(){
+
+    }
+
+    @Test
+    public void queNoPuedaLogearseSiIntroduceMalLaContrasenia(){
+
+    }
+
+    @Test
+    public void queNoSePuedaRegistrarUnMailYaRegistradoYEnvieMensaje(){
+
+    }
+
+}
